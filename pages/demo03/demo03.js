6d9eb326e15f21cd0c5cf78a00d183dd34693dd2
非幡动 <438484791@qq.com>
2020-06-12 21:21:23 +0800

    截至到p15

---
+++ pages/demo03/demo03.js
@@ -0,0 +1,28 @@
+//Page Object
+Page({
+  data: {
+    msg:"hello Daisy",
+    num:10000,
+    isGirl:false,
+    person:{
+      age:20,
+      height:167,
+      weight:100,
+      name:"test"
+    },
+    list:[
+      {
+        id:0,
+        name:"dadada"
+      },
+      {
+        id:1,
+        name:"dududu"
+      },
+      {
+        id:2,
+        name:"bilibili"
+      }
+    ]
+  }
+});
